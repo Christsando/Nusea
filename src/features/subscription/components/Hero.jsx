8fda30de78c52e32ff8673971973dd4be0d3c912
Wisnu-putra123 <wisnuputra204@gmail.com>
2025-09-13 12:03:36 +0700

    Update Hero

--- src/features/subscription/components/Hero.jsx
+++ src/features/subscription/components/Hero.jsx
@@ -6,12 +6,12 @@ export default function Hero() {
       className="h-screen flex flex-col items-center justify-center text-center bg-cover bg-center"
       style={{ backgroundImage: `linear-gradient(to bottom, rgba(255,255,255,0), rgba(255,255,255,0.7)), url(${bannerImg})` }}
     >
-        <div className="bg-black bg-opacity-20 w-full h-full flex flex-col items-center justify-end">
+        <div className="bg-black bg-opacity-10 w-full h-full flex flex-col items-center justify-end">
           <h1 className="text-4xl md:text-6xl font-extrabold text-white drop-shadow-lg">
             SEAFOOD SEGAR <br /><br /> DARI INDONESIA MENUJU DUNIA
           </h1>
         </div>
-        <div className="px-60 bg-black bg-opacity-20 w-full h-full flex flex-col items-start justify-center">
+        <div className="px-60 bg-black bg-opacity-10 w-full h-full flex flex-col items-start justify-center">
           <button className="mt-10 px-6 py-3 bg-orange-500 text-white font-bold rounded-full shadow-lg hover:bg-orange-600">
             Pelajari Selengkapnya
           </button>
