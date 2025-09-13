11f9edb5ebe056d67d07a792ae286ef8cb675579
Christsando <christsandoariel05@gmail.com>
2025-09-13 22:02:44 +0700

    create productCard component and keunggulanCard

--- src/features/marketplace/components/ProductsCard.jsx
+++ src/features/marketplace/components/ProductsCard.jsx
@@ -2,22 +2,28 @@ import "./component.css";
 import { Plus } from "lucide-react";
 
 const ProductsCard = ({ name, price, image }) => {
-  return (
-    <div className="productCard hover:shadow-lg transition">
-        <img 
-            src={image} 
-            alt={name} 
-            className="product-image" 
-        />
-        <h3 className="product-name">{name}</h3>
-        <p className="product-price">{price}</p>
-        <div className="flex justify-end mt-2">
-            <button className="p-2 rounded-full hover:bg-gray-200">
-                <Plus size={18} />
-            </button>
+    return (
+        <div className="productCard">
+            <img
+                src={image}
+                alt={name}
+                className="product-image"
+            />
+
+            <div className="productDetailsContainer">
+                <div className="product-name">
+                    <h3>{name}</h3>
+                </div>
+
+                <div className="priceAndButtonContainer">
+                    <p className="product-price">{price}</p>
+                    <a className="plusButton">
+                        <Plus size={12} className="iconPlus" />
+                    </a>
+                </div>
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default ProductsCard;
--- src/features/marketplace/pages/Market.js
+++ src/features/marketplace/pages/Market.js
@@ -4,32 +4,109 @@ import MarketBanner from '../components/MarketBanner';
 
 
 function App() {
+
+  const productsUnggulan = [
+    { id: 1, name: "Udang", price: "Rp120.000/Kg", image: "" },
+    { id: 2, name: "Ikan Bandeng", price: "Rp50.000/Kg", image: "" },
+    { id: 3, name: "Kepiting", price: "Rp150.000/Kg", image: "" },
+    { id: 4, name: "Paus", price: "Rp100.000.000/Kg", image: "" },
+    { id: 5, name: "Kerang Hijau", price: "Rp100.000/Kg", image: "" },
+    // { id: 6, name: "Kerang", price: "Rp30.000/Kg", image: "" },
+  ];
+
+  const products = [
+    { id: 1, name: "Udang", price: "Rp120.000/Kg", image: "" },
+    { id: 2, name: "Ikan Bandeng", price: "Rp50.000/Kg", image: "" },
+    { id: 3, name: "Kepiting", price: "Rp150.000/Kg", image: "" },
+    { id: 4, name: "Paus", price: "Rp100.000.000/Kg", image: "" },
+    { id: 5, name: "Kerang Hijau", price: "Rp100.000/Kg", image: "" },
+    { id: 6, name: "Kerang", price: "Rp30.000/Kg", image: "" },
+    { id: 7, name: "Udang", price: "Rp120.000/Kg", image: "" },
+    { id: 8, name: "Ikan Bandeng", price: "Rp50.000/Kg", image: "" },
+    { id: 9, name: "Kepiting", price: "Rp150.000/Kg", image: "" },
+    { id: 10, name: "Paus", price: "Rp100.000.000/Kg", image: "" },
+    { id: 11, name: "Kerang Hijau", price: "Rp100.000/Kg", image: "" },
+    { id: 12, name: "Kerang", price: "Rp30.000/Kg", image: "" },
+  ];
+
   return (
     <div className="App">
 
       {/* Title tab here */}
       <title>Nusea | Market</title>
-      
+
       {/* Banner Header */}
-      <header className='marketHeader'>
+      <header>
         <MarketBanner />
       </header>
-      
-      <main className='contentContainer'>
+
+      <main>
 
         {/* TODO : isi content dari carousel card */}
         <div className='productCarousel'>
         </div>
 
+        {/* this is separator */}
+        <h1 className="titleSection">Produk Unggulan</h1>
+
         {/* TODO : Create list, use grid untuk semua product  */}
         <div className='productGrid'>
-          <ProductsCard
-          // TODO : ganti dengan data dari API
-            name="Udang"
-            price="Rp120.000/Kg"
-            image="https://upload.wikimedia.org/wikipedia/commons/0/05/Shrimp_on_white_background.jpg"/>
+          {productsUnggulan.map((product) => (
+            <ProductsCard
+              key={product.id}
+              name={product.name}
+              price={product.price}
+              image={product.image}
+            />
+          ))}
         </div>
-      
+
+        {/* this is separator */}
+        <h1 className="titleSection">Keunggulan Kami</h1>
+
+        {/* TODO : Create grid for keunggulan kami */}
+        <div className='keunggulanGrid'>
+          <div className='keunggulanCard'>
+            <h2>Harga Terbaik</h2>
+            <img src='' alt='icon' />
+            <div>
+              <h2>Kualitas Ekspor</h2>
+              <p>Dapatkan harga terbaik untuk produk laut segar langsung dari nelayan.</p>
+            </div>
+          </div>
+          <div className='keunggulanCard'>
+            <h2>Harga Terbaik</h2>
+            <img src='' alt='icon' />
+            <div>
+              <h2>Kualitas Ekspor</h2>
+              <p>Dapatkan harga terbaik untuk produk laut segar langsung dari nelayan.</p>
+            </div>
+          </div>
+          <div className='keunggulanCard'>
+            <h2>Harga Terbaik</h2>
+            <img src='' alt='icon' />
+            <div>
+              <h2>Kualitas Ekspor</h2>
+              <p>Dapatkan harga terbaik untuk produk laut segar langsung dari nelayan.</p>
+            </div>
+          </div>
+        </div>
+
+        {/* this is separator */}
+        <h1 className="titleSection">List Produk</h1>
+
+        {/* TODO : Create list, use grid untuk semua product  */}
+        <div className='productGrid'>
+          {products.map((product) => (
+            <ProductsCard
+              key={product.id}
+              name={product.name}
+              price={product.price}
+              image={product.image}
+            />
+          ))}
+        </div>
+
       </main>
     </div>
   );
